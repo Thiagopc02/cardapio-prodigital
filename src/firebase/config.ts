6931ef1f7e8ca02e9bb2c2738eddffb99fec0587
Thiagopc02 <thiagotorres5517@gmail.com>
2026-02-04 17:15:52 -0300

    chore: force redeploy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAy4iAWVSc3QZkZb0DyEtqrI_29Wnq8",
-  authDomain: "cardapio-prodigital.firebaseapp.com",
-  projectId: "cardapio-prodigital",
-  storageBucket: "cardapio-prodigital.appspot.com",
-  messagingSenderId: "116283840464",
-  appId: "1:116283840464:web:235e661ebc45a74eb0c85",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
